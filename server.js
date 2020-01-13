66cc2ce10ea940391cbcfa11d8cf6c05fc7c6541
Thiago Duda <thiagodudacc@gmail.com>
2020-01-13 12:06:25 -0300

    Submitting the project

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requiredir = require('require-dir');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi',
+  { useUnifiedTopology: true, useNewUrlParser: true}
+);
+requiredir('./src/models/');
+
+// const Product = mongoose.model('Product');
+
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
